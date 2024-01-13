5c65df698784c18cc99822bef813dcdf1c2b42e5
divynshu <156162995+divynshu@users.noreply.github.com>
2024-01-13 13:27:13 +0530

    Add files via upload

---
+++ Using_data_type & math operators/life_in_weeks.py
@@ -0,0 +1,11 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> KeyboardInterrupt
+>>> KeyboardInterrupt
+>>> #life in weeks
+... age=int(input("enter your age:"))
+... print("If you live up to max 90.")
+... year=90-age
+... weeks=year*52
+... print(f"so you have {weeks} weeks left.")
+... print("So dont waste it...")
