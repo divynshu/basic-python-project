e646e89119f9bbc1cfb9d640ca7f74bf8a6225ee
divynshu <156162995+divynshu@users.noreply.github.com>
2024-01-23 00:53:17 +0530

    Add files via upload

---
+++ using-for-loop-and-range-function/basic range function.py
@@ -0,0 +1,13 @@
+#range(start,stop,step),
+#start is inclusive,
+#stop is exclusive,
+#let print number 1 to 10
+
+print("HORIZONTAL PRINTING")
+for i in range(1,11):
+    print(i)
+    
+    
+print("VERTICAL PRINTING")
+for i in range(1,11):
+    print(i,end=" ")
