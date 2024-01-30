88b29a76afd7e2d3b85b6e2512049f7a7c308abe
divynshu <156162995+divynshu@users.noreply.github.com>
2024-01-30 17:16:33 +0530

    Add files via upload

---
+++ by using user define function/area and perimeter of square.py
@@ -0,0 +1,11 @@
+def area( length):
+    a=2**(length)
+    print("area of square is:",a)
+
+def perimeter(length):
+    a=4*int(length)
+    print("perimeter of square is:",a)
+        
+b=int(input("enter the length of square:"))    
+area(b) # function call for area of square
+perimeter(b)  # function call for perimeter of square
