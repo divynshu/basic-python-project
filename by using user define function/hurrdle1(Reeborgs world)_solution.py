6f259de2e3bac11a00e681ef1fb29a62c9c7f181
divynshu <156162995+divynshu@users.noreply.github.com>
2024-02-01 22:34:53 +0530

    Add files via upload

---
+++ by using user define function/hurrdle1(Reeborgs world)_solution.py
@@ -0,0 +1,22 @@
+# this code in python in Reeborgs world 
+#hardle1 solution
+# make turn_right function ,as it is not given( usser defined fuction)
+def turn_right():
+    turn_left()   #turn_left function given in by program(inbuilt function)
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()        # move and turn left function is inbuilt function for move one step and turn left respectively.
+    turn left()
+    move() 
+    turn left()
+    move() 
+    turn left()
+    move() 
+    turn left()
+    
+#use for loop or write 6 times jump function
+
+for step in range(6):
+    jump()           
