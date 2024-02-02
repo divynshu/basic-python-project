73a3986a6901de58df2a5179c01612fd35b3d581
divynshu <156162995+divynshu@users.noreply.github.com>
2024-02-02 23:27:35 +0530

    Add files via upload

---
+++ by using user define function/simple_user_define function.py
@@ -0,0 +1,12 @@
+# a user define function to print hello world
+
+def function_name():
+    print("hello")
+    print("world")
+
+
+#in function indentation matter
+# the code which is not intended in function is not execute in function call.
+    
+#we have to call the function to execute function inner code, AS just by writting function name.
+function_name()
